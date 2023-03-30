e77beb27ff1b9a9e034ddb0921fdd8414ea8f8e6
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-03-30 21:38:10 +0530

    change the data

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const axios = require('axios');
 const cors = require('cors');
 // configs
-API_KEY = '2c569aae15f54917a0c8ecfd02bec6db';
+// API_KEY = '2c569aae15f54917a0c8ecfd02bec6db';
+// API_KEY = '6f75e303644c4cd486bf940475f6c441';
+API_KEY = '131f49c708294ce8bc1db138e1087034';
 
 // APP USE
 app.use(bodyParser.json());
@@ -16,27 +18,40 @@ app.use(cors());
 app.post('/random', (req, res) => {
   //vegetarian
   const target = req.body.target || '';
+  const number = req.body.number || 10;
   const fetchRandom = async () => {
     const response = await axios.get(
-      `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}&tags=${target}`
+      `https://api.spoonacular.com/recipes/random?number=${number}&apiKey=${API_KEY}&tags=${target}`
     );
 
     const randomResult = await response.data.recipes;
-    console.log(randomResult);
     res.json(randomResult);
   };
   fetchRandom();
+});
+
+// RESPONSE OF RECIPE INFORMATION
+app.post('/recipe', (req, res) => {
+  const id = req.body.id;
+  const fetchInfo = async () => {
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`
+    );
+    const recipeResult = response;
+    console.log(recipeResult.data);
+    // console.log(recipeResult);
+    res.json(recipeResult.data);
+  };
+  fetchInfo();
 });
 
 // RESPONSE BASED ON SEARCH
 app.post('/search', (req, res) => {
   const target = req.body.target;
   const fetchSearch = async () => {
-    // console.log('req recived');
     const response = await axios.get(
       `https://api.spoonacular.com/recipes/complexSearch?query=${target}&apiKey=${API_KEY}`
     );
-    // console.log(response);
     const searchResult = await response.data;
     res.json(searchResult);
   };
@@ -49,7 +64,7 @@ app.post('/cuisine/:name', (req, res) => {
   const cuisine = req.params.name;
   const fetchCuisine = async () => {
     const response = await axios.get(
-      `https://api.spoonacular.com/recipes/complexSearch?cuisine=${cuisine}&apiKey=6f75e303644c4cd486bf940475f6c441`
+      `https://api.spoonacular.com/recipes/complexSearch?cuisine=${cuisine}&apiKey=${API_KEY}`
     );
     const cuisineResult = await response.data;
     res.json(cuisineResult);
